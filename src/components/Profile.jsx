af75d9325449cca785ba71b4af2d0c5acc2815d3
Akira Abe <akiraabe2005@yahoo.co.jp>
2022-06-15 12:00:02 +0000

    show profile screen

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,11 +1,40 @@
+import { DataStore } from 'aws-amplify';
+import { User } from '../models';
 import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import ProfileCard from '../ui-components/ProfileCard';
 
 export const Profile = () => {
   const params = useParams();
   console.log(params);
   const id = params.userId;
+
+  const [user, setUser] = useState(null);
+
+  const getUser = async () => {
+    const user = await DataStore.query(User, id);
+    console.log(user);
+    setUser(user);
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
   return (
-  <div>Profile {id} </div>
+    <>
+      <div>UserId: {id} </div>
+      <ProfileCard
+        user={user}
+        overrides={{
+          Button: {
+            onClick: (e) => {
+              console.log('button was clicked.');
+            },
+          },
+        }}
+      />
+    </>
   );
 };
 
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -3,11 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Loading } from '../components/Loading';
 
 const Top = lazy(() => import('../components/Top'));
-const Profiles = lazy(() => import('../components/Profiles'));
 const Profile = lazy(() => import('../components/Profile'));
-// import Top from '../components/Top';
-// import Profiles from '../components/Profiles';
-// import Profile from '../components/Profile';
 
 export const Router = () => {
   return (
@@ -16,9 +12,7 @@ export const Router = () => {
         <Routes>
           <Route path='' element={<Top />} />
           <Route path='/' element={<Top />} />
-          <Route path='/users' element={<Profiles />}>
-            <Route path=':userId' element={<Profile />} />
-          </Route>
+          <Route path='/users/:userId' element={<Profile />} />
         </Routes>
       </Suspense>
     </BrowserRouter>
