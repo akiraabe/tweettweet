34a22eba14d5b85a8d52c3255dfd2c97581c9add
Akira Abe <akiraabe2005@yahoo.co.jp>
2022-06-20 23:27:28 +0000

    I fixed format of the code

--- src/components/Top.jsx
+++ src/components/Top.jsx
@@ -3,7 +3,7 @@ import { Like, Post } from '../models';
 
 import { useEffect, useState } from 'react';
 import { CommentCard } from '../ui-components';
-import { Collection } from "@aws-amplify/ui-react";
+import { Collection } from '@aws-amplify/ui-react';
 function Top({ cognitoUser }) {
   const [posts, setPosts] = useState([]);
 
@@ -20,97 +20,97 @@ function Top({ cognitoUser }) {
 
   return (
     <div>
-<Collection
-      type="list"
-      isSearchable={true}
-      isPaginated={true}
-      searchPlaceholder="Search..."
-      itemsPerPage={6}
-      direction="column"
-      justifyContent="stretch"
-      items={posts || []}
-    >
-      {(post, index) => (
-        <CommentCard
-          post={post}
-          key={post.id}
-          user={post.User}
-          overrides={{
-            Share: {
-              onClick: async (e) => {
-                e.preventDefault();
+      <Collection
+        type='list'
+        isSearchable={true}
+        isPaginated={true}
+        searchPlaceholder='Search...'
+        itemsPerPage={6}
+        direction='column'
+        justifyContent='stretch'
+        items={posts || []}
+      >
+        {(post, index) => (
+          <CommentCard
+            post={post}
+            key={post.id}
+            user={post.User}
+            overrides={{
+              Share: {
+                onClick: async (e) => {
+                  e.preventDefault();
 
-                // Postをid指定で取得する
-                const postToChange = await DataStore.query(Post, post.id);
+                  // Postをid指定で取得する
+                  const postToChange = await DataStore.query(Post, post.id);
 
-                // post.UserとcognitoUserが同じか判定する。
-                console.log(post.User.accountName);
-                console.log(cognitoUser.username);
-                if (post.User.accountName === cognitoUser.username) {
-                  console.log('same user');
-                  return;
-                } else {
-                  console.log('another user');
-                }
+                  // post.UserとcognitoUserが同じか判定する。
+                  console.log(post.User.accountName);
+                  console.log(cognitoUser.username);
+                  if (post.User.accountName === cognitoUser.username) {
+                    console.log('same user');
+                    return;
+                  } else {
+                    console.log('another user');
+                  }
 
-                // Likeテーブルを探す
-                const likes = await DataStore.query(Like);
-                console.log(likes);
-                const like = likes
-                  .filter((obj) => obj.likedBy === cognitoUser.username)
-                  .filter((obj) => obj.Post.id === post.id);
-                console.log('like...');
-                console.log(like[0]);
-                const like0 = like[0];
+                  // Likeテーブルを探す
+                  const likes = await DataStore.query(Like);
+                  console.log(likes);
+                  const like = likes
+                    .filter((obj) => obj.likedBy === cognitoUser.username)
+                    .filter((obj) => obj.Post.id === post.id);
+                  console.log('like...');
+                  console.log(like[0]);
+                  const like0 = like[0];
 
-                if (like0 && !like0.deleted) {
-                  // count down
-                  await DataStore.save(
-                    Post.copyOf(postToChange, (updated) => {
-                      updated.likes -= 1;
-                    })
-                  );
-                  // Likeテーブル更新
-                  await DataStore.save(
-                    Like.copyOf(like0, (updated) => {
-                      updated.deleted = true;
-                    })
-                  );
-                } else {
-                  if (like.length === 0) {
-                    // Likeテーブルを作る
-                    console.log('SaveLike');
-                    console.log(post);
+                  if (like0 && !like0.deleted) {
+                    // count down
                     await DataStore.save(
-                      new Like({
-                        Post: post,
-                        deleted: false,
-                        likedBy: cognitoUser.username,
+                      Post.copyOf(postToChange, (updated) => {
+                        updated.likes -= 1;
                       })
                     );
-                  } else {
                     // Likeテーブル更新
                     await DataStore.save(
                       Like.copyOf(like0, (updated) => {
-                        updated.deleted = false;
+                        updated.deleted = true;
+                      })
+                    );
+                  } else {
+                    if (like.length === 0) {
+                      // Likeテーブルを作る
+                      console.log('SaveLike');
+                      console.log(post);
+                      await DataStore.save(
+                        new Like({
+                          Post: post,
+                          deleted: false,
+                          likedBy: cognitoUser.username,
+                        })
+                      );
+                    } else {
+                      // Likeテーブル更新
+                      await DataStore.save(
+                        Like.copyOf(like0, (updated) => {
+                          updated.deleted = false;
+                        })
+                      );
+                    }
+                    console.log('SaveLike # after');
+
+                    // count up
+                    await DataStore.save(
+                      Post.copyOf(postToChange, (updated) => {
+                        updated.likes += 1;
                       })
                     );
                   }
-                  console.log('SaveLike # after');
-
-                  // count up
-                  await DataStore.save(
-                    Post.copyOf(postToChange, (updated) => {
-                      updated.likes += 1;
-                    })
-                  );
-                }
-                getPosts();
+                  getPosts();
+                },
               },
-            },
-          }}
-        />
-      )}
+            }}
+          />
+        )}
       </Collection>
     </div>
   );
