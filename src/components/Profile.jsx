5c9979975a4e8d9e0daaf79e7949602fbcbae187
Akira Abe <abe.akira@tis.co.jp>
2022-08-08 10:18:58 +0900

    added call the api

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,6 +3,7 @@ import { User } from '../models';
 import { useParams , useNavigate} from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import ProfileCard from '../ui-components/ProfileCard';
+import axios from 'axios';
 
 export const Profile = () => {
   const params = useParams();
@@ -16,6 +17,16 @@ export const Profile = () => {
   const getUser = async () => {
     const user = await DataStore.query(User, id);
     // console.log(user);
+    const instance = await axios.request({
+      method: 'get',
+      url: `https://s0iszi2di2.execute-api.ap-northeast-1.amazonaws.com/dev/users/${id}`,
+      crossDomain: true
+
+    });
+    //const response = await instance.get();
+    console.log(`https://s0iszi2di2.execute-api.ap-northeast-1.amazonaws.com/dev/users/${id}`)
+    console.log('instance', instance);
+
     setUser(user);
   };
 
